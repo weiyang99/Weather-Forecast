408fd7f6369a79e28b49b34e65388d113a871e94
weiyang <weiyangwygoh@gmail.com>
2023-02-24 11:14:39 +0800

    filled template

--- src/App.js
+++ src/App.js
@@ -1,8 +1,62 @@
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q={${location}}&appid={${process.env.API_KEY}}`
+
+  const search = (e) => {
+    if (e.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+      })
+      setLocation('')
+    }
+  }
+
   return (
     <div className="App">
+      <div className='search'>
+        <input
+          value={location}
+          type='text'
+          onChange={(e) => setLocation(e.target.value)}
+          onKeyPress={search}
+          placeholder='Enter Location' />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        }
+      </div>
     </div>
   );
 }
