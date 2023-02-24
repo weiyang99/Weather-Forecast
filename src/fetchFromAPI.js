25026e31d3bbfc1465094ddc54fcf85fd6df3408
weiyang <weiyangwygoh@gmail.com>
2023-02-24 18:03:14 +0800

    hide

--- src/fetchFromAPI.js
+++ src/fetchFromAPI.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+var REACT_APP_RAPID_API_KEY = config.REACT_APP_RAPID_API_KEY;
+
 const BASE_URL = 'https://open-weather13.p.rapidapi.com/city'
 
 const options = {
     headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+        'X-RapidAPI-Key': REACT_APP_RAPID_API_KEY,
         'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
     }
 };
