9e39ef33144266b62bdb8260ecf498b1cf2542b4
weiyang <weiyangwygoh@gmail.com>
2023-02-26 09:15:49 +0800

    update units

--- src/fetchFromAPI.js
+++ src/fetchFromAPI.js
@@ -11,7 +11,7 @@ const options = {
 };
 
 export const fetchFromAPI = async (url) => {
-    const { data } = await axios.get(`${BASE_URL}/${url}`, options)
+    const { data } = await axios.get(`${BASE_URL}/${url}&units=metric`, options)
 
     return data
 }
